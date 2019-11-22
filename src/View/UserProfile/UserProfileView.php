c56810e417c0407754dcb850cafb82859c6a04ee
Szymon Kaminski <szymon.kaminski@bitbag.pl>
2019-11-22 18:03:44 +0100

    Add AddressView, update UserProfileView

--- src/View/UserProfile/UserProfileView.php
+++ src/View/UserProfile/UserProfileView.php
@@ -12,31 +12,46 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusVueStorefrontPlugin\View\UserProfile;
 
+use BitBag\SyliusVueStorefrontPlugin\View\AddressView;
+
 final class UserProfileView
 {
+    /** @var int */
     public $id;
 
+    /** @var int */
     public $group_id;
 
+    /** @var string */
     public $default_shipping;
 
+    /** @var string */
     public $created_at;
 
+    /** @var string */
     public $updated_at;
 
+    /** @var string */
     public $created_in;
 
+    /** @var string */
     public $email;
 
+    /** @var string */
     public $firstname;
 
+    /** @var string */
     public $lastname;
 
+    /** @var int */
     public $store_id;
 
+    /** @var int */
     public $website_id;
 
+    /** @var array|AddressView[] */
     public $addresses;
 
+    /** @var int */
     public $disable_auto_group_change;
 }
