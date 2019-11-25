746e15b146d6d25fb8cb9651768e903fc67d0748
pawel <heros68@wp.pl>
2019-11-25 17:26:39 +0100

    #Add /user/reset-password - create logic for reset password; sent new link password

---
+++ src/Sylius/Mailer/Emails.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusVueStorefrontPlugin\Sylius\Mailer;
+
+final class Emails
+{
+    public const EMAIL_VERIFICATION_TOKEN = 'api_verification_token';
+
+    public const EMAIL_RESET_PASSWORD_TOKEN = 'api_reset_password_token';
+
+    public const EMAIL_ORDER_CONFIRMATION = 'api_order_confirmation';
+
+    private function __construct()
+    {
+    }
+}
