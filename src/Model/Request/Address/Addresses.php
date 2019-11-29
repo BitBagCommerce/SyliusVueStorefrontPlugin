18cf2b81deab2ab8ed77762d9e78f9b21db725d7
pawel <heros68@wp.pl>
2019-11-29 15:38:21 +0100

    #Add /user/me update bitbag doc; set class as final

--- src/Model/Request/Address/Addresses.php
+++ src/Model/Request/Address/Addresses.php
@@ -1,10 +1,18 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * another great project.
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusVueStorefrontPlugin\Model\Request\Address;
 
-class Addresses
+final class Addresses
 {
     private const ID = 'id';
     private const CUSTOMER_ID = 'customer_id';
