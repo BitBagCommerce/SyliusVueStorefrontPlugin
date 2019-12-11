84971db9a65207b911d9cb6853971dfddfe8bfee
pawel <heros68@wp.pl>
2019-12-11 13:17:06 +0100

    Api/cart/shipping-methods

--- src/Controller/Cart/SetShippingMethodsAction.php
+++ src/Controller/Cart/SetShippingMethodsAction.php
@@ -12,16 +12,71 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusVueStorefrontPlugin\Controller\Cart;
 
+use BitBag\SyliusVueStorefrontPlugin\Factory\Cart\ShippingMethodsViewFactoryInterface;
+use BitBag\SyliusVueStorefrontPlugin\Factory\GenericSuccessViewFactoryInterface;
+use BitBag\SyliusVueStorefrontPlugin\Factory\ValidationErrorViewFactoryInterface;
+use BitBag\SyliusVueStorefrontPlugin\Request\Cart\SetShippingMethodsRequest;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\View\ViewHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class SetShippingMethodsAction
 {
-    public function __construct()
-    {
+    /** @var MessageBusInterface */
+    private $bus;
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    /** @var ViewHandlerInterface */
+    private $viewHandler;
+
+    /** @var ValidationErrorViewFactoryInterface */
+    private $validationErrorViewFactory;
+
+    /** @var GenericSuccessViewFactoryInterface */
+    private $genericSuccessViewFactory;
+
+    /** @var ShippingMethodsViewFactoryInterface */
+    private $shippingMethodsViewFactory;
+
+    public function __construct(
+        MessageBusInterface $bus,
+        ValidatorInterface $validator,
+        ViewHandlerInterface $viewHandler,
+        ValidationErrorViewFactoryInterface $validationErrorViewFactory,
+        GenericSuccessViewFactoryInterface $genericSuccessViewFactory,
+        ShippingMethodsViewFactoryInterface $shippingMethodsViewFactory
+    ) {
+        $this->bus = $bus;
+        $this->validator = $validator;
+        $this->viewHandler = $viewHandler;
+        $this->validationErrorViewFactory = $validationErrorViewFactory;
+        $this->genericSuccessViewFactory = $genericSuccessViewFactory;
+        $this->shippingMethodsViewFactory = $shippingMethodsViewFactory;
     }
 
     public function __invoke(Request $request): Response
     {
+        $shippingMethodsRequest = SetShippingMethodsRequest::fromHttpRequest($request);
+
+        $validationResults = $this->validator->validate($shippingMethodsRequest);
+
+        if (0 !== count($validationResults)) {
+            return $this->viewHandler->handle(View::create(
+                $this->validationErrorViewFactory->create($validationResults),
+                Response::HTTP_BAD_REQUEST
+            ));
+        }
+
+        $this->bus->dispatch($shippingMethodsRequest->getCommand());
+
+        return $this->viewHandler->handle(View::create(
+            $this->genericSuccessViewFactory->create($this->shippingMethodsViewFactory->createList()),
+            Response::HTTP_OK
+        ));
     }
 }
