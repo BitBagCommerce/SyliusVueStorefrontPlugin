da0924fbcad2b932720a65cd0040840f0b4257b2
Szymon Kaminski <szymon.kaminski@bitbag.pl>
2019-12-12 16:33:22 +0100

    Update RequestProcessor with class names, add its interface, service definitions

--- src/Processor/RequestProcessor.php
+++ src/Processor/RequestProcessor.php
@@ -12,26 +12,30 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusVueStorefrontPlugin\Processor;
 
-use BitBag\SyliusVueStorefrontPlugin\Request\User\CreateUserRequest;
+use BitBag\SyliusVueStorefrontPlugin\Command\CommandInterface;
+use BitBag\SyliusVueStorefrontPlugin\Query\QueryInterface;
+use BitBag\SyliusVueStorefrontPlugin\Request\RequestInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-final class RequestProcessor
+final class RequestProcessor implements RequestProcessorInterface
 {
-    private const FORMAT_JSON = 'json';
-
     /** @var ValidatorInterface */
     private $validator;
 
     /** @var SerializerInterface */
     private $serializer;
 
-    public function __construct(ValidatorInterface $validator, SerializerInterface $serializer)
+    /** @var string */
+    private $className;
+
+    public function __construct(ValidatorInterface $validator, SerializerInterface $serializer, string $className)
     {
         $this->validator = $validator;
         $this->serializer = $serializer;
+        $this->className = $className;
     }
 
     public function validate(Request $httpRequest): ConstraintViolationListInterface
@@ -39,17 +43,22 @@ final class RequestProcessor
         return $this->validator->validate($this->transformHttpRequest($httpRequest));
     }
 
-    public function getCommand(Request $httpRequest, string $className): object
+    public function getCommand(Request $httpRequest): CommandInterface
     {
         return $this->transformHttpRequest($httpRequest)->getCommand();
     }
 
-    private function transformHttpRequest(Request $httpRequest): object
+    public function getQuery(Request $httpRequest): QueryInterface
+    {
+        return $this->transformHttpRequest($httpRequest)->getQuery();
+    }
+
+    private function transformHttpRequest(Request $httpRequest): RequestInterface
     {
         $requestBody = $this->serializer->decode($httpRequest->getContent(), self::FORMAT_JSON);
         return $this->serializer->denormalize(
             \array_merge($requestBody, $httpRequest->attributes->all(), $httpRequest->query->all()),
-            CreateUserRequest::class
+            $this->className
         );
     }
 }
