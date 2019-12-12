3b92d27b9104b978299e901101dd1fa2ef0ecdfd
Szymon Kaminski <szymon.kaminski@bitbag.pl>
2019-12-12 16:34:57 +0100

    Add Query interface and its classes for stock endpoints

---
+++ src/Query/Stock/CheckStock.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ *  Feel free to contact us once you face any issues or want to start
+ *  another great project.
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusVueStorefrontPlugin\Query\Stock;
+
+use BitBag\SyliusVueStorefrontPlugin\Query\QueryInterface;
+
+final class CheckStock implements QueryInterface
+{
+    /** string */
+    private $sku;
+
+    public function __construct(string $sku)
+    {
+        $this->sku = $sku;
+    }
+
+    public function sku(): string
+    {
+        return $this->sku;
+    }
+}
