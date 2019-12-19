6e3e6556b5ad025cf7b8b6db3c37278389c74abe
pawel <heros68@wp.pl>
2019-12-19 16:54:39 +0100

    create query request

---
+++ src/Query/Cart/ShippingMethod.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * another great project.
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusVueStorefrontPlugin\Query\Cart;
+
+use BitBag\SyliusVueStorefrontPlugin\Model\Request\Common\Address;
+use BitBag\SyliusVueStorefrontPlugin\Query\QueryInterface;
+
+final class ShippingMethod implements QueryInterface
+{
+    /** @var string */
+    private $token;
+
+    /** @var int|string */
+    private $cartId;
+
+    /** @var Address */
+    private $address;
+
+    public function __construct(
+        string $token,
+        $cartId,
+        Address $address
+    ) {
+        $this->token = $token;
+        $this->cartId = $cartId;
+        $this->address = $address;
+    }
+
+    public function cartId()
+    {
+        return $this->cartId;
+    }
+
+    public function Address(): Address
+    {
+        return $this->address;
+    }
+}
