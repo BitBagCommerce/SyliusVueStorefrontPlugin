9fbd77c2769bcc6d5468c799fdde46bc6411406d
Offcurve <rytlewski.ireneusz@gmail.com>
2019-12-30 11:08:46 +0100

    Update 'DeleteCartActionTest'

--- tests/Functional/Api/DeleteCartActionTest.php
+++ tests/Functional/Api/DeleteCartActionTest.php
@@ -9,7 +9,34 @@ use Tests\BitBag\SyliusVueStorefrontPlugin\Functional\Configuration;
 
 final class DeleteCartActionTest extends JsonApiTestCase
 {
-    public function test_deleting_cart(): void
+    public function test_deleting_cart_item(): void
+    {
+        $this->markTestIncomplete();
+
+        $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'order.yml', 'coupon_based_promotion.yml']);
+
+        $data =
+<<<JSON
+        {
+            "username": "test@example.com",
+            "password": "MegaSafePassword"
+        }
+JSON;
+
+        $this->client->request('POST', '/vsbridge/user/login', [], [], Configuration::CONTENT_TYPE_HEADER, $data);
+
+        $response = $this->client->getResponse();
+
+        $content = json_decode($response->getContent());
+
+        $this->client->request('POST', sprintf('/vsbridge/cart/delete?token=%d&cartId=%d', $content->result, 12345), [], [], Configuration::CONTENT_TYPE_HEADER);
+
+        $response = $this->client->getResponse();
+
+        self::assertResponse($response, 'Controller/Cart/delete_cart_item_successful');
+    }
+
+    public function test_deleting_cart_item_for_blank_item(): void
     {
         $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'order.yml', 'coupon_based_promotion.yml']);
 
@@ -31,6 +58,47 @@ JSON;
 
         $response = $this->client->getResponse();
 
-        self::assertResponse($response, 'Controller/Cart/delete_cart_successful');
+        self::assertResponse($response, 'Controller/Cart/delete_cart_item_blank_item', 500);
+    }
+
+    public function test_deleting_cart_item_for_non_existent_item(): void
+    {
+        $this->loadFixturesFromFiles(['channel.yml', 'customer.yml', 'order.yml', 'coupon_based_promotion.yml']);
+
+        $data =
+<<<JSON
+        {
+            "username": "test@example.com",
+            "password": "MegaSafePassword"
+        }
+JSON;
+
+        $this->client->request('POST', '/vsbridge/user/login', [], [], Configuration::CONTENT_TYPE_HEADER, $data);
+
+        $response = $this->client->getResponse();
+
+        $content = json_decode($response->getContent());
+
+        $data =
+<<<JSON
+        { 
+            "cartItem": 
+                { 
+                    "sku": "Non-existent item",
+                    "qty": 2,
+                    "quoteId": "12345" 
+                }
+        }
+JSON;
+
+        $this->client->request('POST', sprintf(
+            '/vsbridge/cart/delete?token=%d&cartId=%d',
+            $content->result,
+            12345
+        ), [], [], Configuration::CONTENT_TYPE_HEADER, $data);
+
+        $response = $this->client->getResponse();
+
+        self::assertResponse($response, 'Controller/Cart/delete_cart_item_non_existent_item', 400);
     }
 }
